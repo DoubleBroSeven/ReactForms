05bb69ff19bdd496706b9e0420fb659e5a217fbf
Matthew Torma <tormamatthew@gmail.com>
2024-11-07 01:37:26 -0600

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import SignUpForm from "./Components/SignUpForm.jsx";
+import Authenticate from "./Components/Authenticate.jsx";
+import { useState } from 'react';
 
 function App() {
-  
+  const [token, setToken] = useState(null);
 
   return (
     <>
+      <SignUpForm token={token} setToken={setToken} />
+      <Authenticate token={token} setToken={setToken} />
       
     </>
   )
